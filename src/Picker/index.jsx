95c799e4d82a2667ef82a78f544b238fee409bb1
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-01-19 17:07:46 -0300

    feat: import picker

---
+++ src/Picker/index.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Picker } from '@react-native-picker/picker'
+
+export function PickerItem(){
+  return(
+    <Picker>
+      <Picker.Item value="BTC" key={0} label="BTC"/>
+    </Picker>
+  )
+}
