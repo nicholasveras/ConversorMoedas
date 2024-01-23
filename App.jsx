95f662096317db3505596fef55b3020a9f37ce0a
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-01-23 13:12:27 -0300

    feat: input component

--- App.jsx
+++ App.jsx
@@ -1,8 +1,44 @@
+import {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import { PickerItem } from './src/Picker';
+import {api} from './src/services/api'
 
 export default function App() {
+  const [moedas, setMoedas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadMoedas(){
+      const response = await api.get("all")
+      let arrayMoedas = [];
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key,
+        })
+      })
+
+
+      console.log("==========================================")
+      console.log(arrayMoedas);
+
+      setMoedas(arrayMoedas);
+      setLoading(false);
+    }
+
+    loadMoedas();
+  }, [])
+
+  if(loading){
+    return(
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <ActivityIndicator color="#FFFF" size="large"/>
+      </View>
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.areaMoeda}>
